5da069db197e202f9110c085c5d506a54fefa1b6
Alysson Eduardo <alysson6801@hotmail.com>
2021-02-14 12:00:28 -0300

    (14/02) Proteção de Rotas.

--- routes/routes.js
+++ routes/routes.js
@@ -6,10 +6,10 @@ var UserController = require("../controllers/UserController")
 const AdminAuth = require("../middleware/adminAuth")
 
 router.get('/', HomeController.index);
-router.post('/user', UserController.create);
+router.post('/user', AdminAuth,UserController.create);
 router.get("/user", AdminAuth, UserController.index)
-router.get("/user/:id", UserController.findUser)
-router.put("/user", UserController.edit)
+router.get("/user/:id", AdminAuth, UserController.findUser)
+router.put("/user", AdminAuth,UserController.edit)
 router.delete("/user/:id", UserController.remove)
 router.post("/recoverpassword", UserController.recoverPassword)
 router.post("/changepassword", UserController.changePassword)
